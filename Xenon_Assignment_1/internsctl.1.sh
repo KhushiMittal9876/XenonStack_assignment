9f51ae4b328d694f5e412605a6e95af00e5b2dbb
KhushiMittal9876 <76116771+KhushiMittal9876@users.noreply.github.com>
2024-01-10 16:22:29 +0530

    Add files via upload

---
+++ Xenon_Assignment_1/internsctl.1.sh
@@ -0,0 +1,36 @@
+.TH INTERNSCTL 1 "January 2024"
+.SH NAME
+internsctl \- Custom Linux Command for Interns
+.SH SYNOPSIS
+.B internsctl
+[\fIOPTION\fR]
+.SH DESCRIPTION
+.B internsctl
+is a custom Linux command that provides various functionalities for interns.
+.PP
+Available options include:
+.TP
+\fB\-\-help\fR
+Display usage information.
+.TP
+\fB\-\-version\fR
+Display the command version.
+.PP
+.SH OPTIONS
+.TP
+\fB\-\-help\fR
+Display this help message.
+.TP
+\fB\-\-version\fR
+Display the version of internsctl.
+.PP
+.SH EXAMPLES
+Display the version of internsctl:
+.RS
+.B internsctl \-\-version
+.RE
+.PP
+.SH SEE ALSO
+.BR bash (1)
+.SH AUTHOR
+Khushi Mittal
